4af73d7ecefc376880198a1901bc2e314b54ba3a
João Pedro <jpedrodaibello1999@outlook.com>
2025-11-20 21:37:33 -0300

    feat: add race detection and mutex for concurrent job processing

--- application/services/job_manager.go
+++ application/services/job_manager.go
@@ -70,7 +70,9 @@ func (j *JobManager) Start(ch *amqp.Channel) {
 }
 
 func (j *JobManager) NotifySuccess(jobResult JobWorkerResult, ch *amqp.Channel) error {
+	Mutex.Lock()
 	jobJson, err := json.Marshal(jobResult.Job)
+	Mutex.Unlock()
 
 	if err != nil {
 		return err
